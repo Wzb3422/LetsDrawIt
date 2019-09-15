9bde0928d6c2b3b7c14cfdccd58e5a00576b75fb
wzb <wzb3422@gmail.com>
2019-09-15 10:09:55 +0800

    :art: finish Ranks layout

--- src/pages/Rank/components/EntryItem/index.jsx
+++ src/pages/Rank/components/EntryItem/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import heart from './images/heart.png'
+import 'animate.css/animate.min.css'
 import './style.css'
 
 function EntryItem() {
+
+  const [isActivated, setIsActivated] = useState(false)
+
+  const itemRedHeartClazz = 'item-heart gray-heart animated heartBeat'
+  const itemGreyHeartClazz = 'item-heart animated rubberBand'
+
+  const toggleLike = () => {
+    if (isActivated) {
+      // Liked
+      setIsActivated(false)
+    } else {
+      // not yet
+      setIsActivated(true)
+    }
+  }
+
   return (
-    <div className='outline'>
-      {/*<img src="" alt=""/>*/}
+    <div className='outline' onClick={toggleLike}>
+      <img className={isActivated ? itemRedHeartClazz : itemGreyHeartClazz} src={heart} alt="heart"/>
     </div>
   )
 }
