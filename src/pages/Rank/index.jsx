c7eacc8bf031c9d6e33098dd28995b3225b548e3
wzb <wzb3422@gmail.com>
2019-09-20 17:06:14 +0800

    update

--- src/pages/Rank/index.jsx
+++ src/pages/Rank/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import title from './images/title.png'
+import one from './images/1.png'
+import two from './images/2.png'
+import three from './images/3.png'
+import nextTurn from './images/nextTurn.png'
 import './rank-style.css'
 
 function Rank() {
@@ -7,10 +11,20 @@ function Rank() {
     <div className='rank-container'>
       <img className='rank-title' src={title} alt=""/>
       <div className='rank-heroes'>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className='rank-box'>
+          <img className='rank-crown' src={two} alt=""/>
+          <div className='rank-outline'></div>
+        </div>
+        <div className='rank-box'>
+          <img className='rank-crown' src={one} alt=""/>
+          <div className='rank-outline'></div>
+        </div>
+        <div className='rank-box'>
+          <img className='rank-crown' src={three} alt=""/>
+          <div className='rank-outline'></div>
+        </div>
       </div>
+      <img className='next-turn' src={nextTurn} alt=""/>
     </div>
   )
 }
