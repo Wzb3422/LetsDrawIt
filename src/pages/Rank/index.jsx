9f9715bee33a103d2110b5a06d9222886fc04d34
wzb <wzb3422@gmail.com>
2019-09-20 17:15:30 +0800

    update

--- src/pages/Rank/index.jsx
+++ src/pages/Rank/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import webSocket from 'socket.io-client'
 import title from './images/title.png'
 import one from './images/1.png'
 import two from './images/2.png'
@@ -7,6 +8,36 @@ import nextTurn from './images/nextTurn.png'
 import './rank-style.css'
 
 function Rank() {
+
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    setSocket(webSocket('http://101.132.107.146'))
+  }, [])
+
+  useEffect(() => {
+    if (socket) {
+      console.log('connected')
+      console.log(socket)
+      wsInit()
+    }
+  }, [socket])
+
+  const wsInit = () => {
+    socket.on('connect', () => {
+      console.log(`Ws connected as id ${socket.id}`)
+    })
+    socket.on('next', res => {
+      console.log(res)
+    })
+  }
+
+  const toNextTurn = () => {
+    socket.emit('next', {
+      token: window.localStorage.getItem("token")
+    })
+  }
+
   return (
     <div className='rank-container'>
       <img className='rank-title' src={title} alt=""/>
@@ -24,7 +55,7 @@ function Rank() {
           <div className='rank-outline'></div>
         </div>
       </div>
-      <img className='next-turn' src={nextTurn} alt=""/>
+      <img className='next-turn' src={nextTurn} onClick={toNextTurn} alt="nextTurn"/>
     </div>
   )
 }
